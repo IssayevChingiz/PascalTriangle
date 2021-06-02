aaf90a8f08a6dbd394a49e436fb745dc08d6f45c
IssayevChingiz <46941620+IssayevChingiz@users.noreply.github.com>
2021-06-01 21:12:05 -0400

    two dimensional array display test

--- src/Triangle.js
+++ src/Triangle.js
@@ -1,9 +1,21 @@
 import { useParams, Link } from "react-router-dom";
 export default function Triangle(props) {
   let { num } = useParams();
+  let arr = [[1], [23, 34], [45, 67, 78]];
   return (
     <div>
       <h3>Num of rows: {num}</h3>
+
+      {arr.map((items, index) => {
+        return (
+          <div>
+            {items.map((subItems, sIndex) => {
+              return <div> {subItems} </div>;
+            })}
+            ----------------------
+          </div>
+        );
+      })}
     </div>
   );
 }
